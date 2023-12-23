3c6c1569c2936ec5b9c66987a3ff1f56f3bca107
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-12-22 19:17:54 -0500

    Actualizaciones proyecto

--- sketch.js
+++ sketch.js
@@ -19,23 +19,20 @@ function draw() {
     let xoff = 0; // variable xoff para el ruido en el eje X
     for (let x = 0; x < columnas; x++) {
       let index = x + y * columnas; //Calculo de índice para cada posición en la cuadrícula
-      //let angle = noise(xoff, yoff) * TWO_PI * 2; // Generación de ángulo basado en el ruido
       let size = map(noise(xoff, yoff), 0, 1, -10, 25); // Generación de tamaño basado en el ruido
 
-      // Dibujo de una cruz
+      // Dibujo de las cruces
       drawCruz(x * scl, y * scl, size); //angle
 
-      xoff += 0.05; // Ajuste de la suavidad de la transición entre valores de ruido en el eje X
+      xoff += 0.04; // Ajuste de la suavidad de la transición entre valores de ruido en el eje X
     }
-    yoff += 0.05; // Ajuste de la suavidad de la transición entre valores de ruido en el eje Y
+    yoff += 0.04; // Ajuste de la suavidad de la transición entre valores de ruido en el eje Y
   }
 }
 
 function drawCruz(x, y, size) {
-  //angle
   push();
   translate(x, y); // Translación del origen a la posición (x, y)
-  //rotate(angle);
   line(-size, 0, size, 0); // línea horizontal de la cruz
   line(0, -size, 0, size); // línea vertical de la cruz
   pop(); //Restauración de la configuración de transformación previa
